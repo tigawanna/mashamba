f6bee8313ea71c1f5a40ca94f4add387a3e0ab87
tigawanna <denniskinuthiaw@gmail.com>
2023-02-26 19:22:49 +0300

    build

--- .vercel/output/static/assets/index-426a2735.js
+++
@@ -1,3 +0,0 @@
-import { s as t } from "./client-c8939a82.js";
-import "./index-e76fa941.js";
-t();
--- .vercel/output/static/assets/landscapp-9cde5c42.js
+++ .vercel/output/static/assets/landscapp-9cde5c42.js
@@ -1,2 +1,1 @@
-const a = "/assets/landscapp-4c66a0b2.svg";
-export { a as L };
+const a="/assets/landscapp-4c66a0b2.svg";export{a as L};
