7954e662766ebfcbe688bb29c3caae82ee37570d
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-12-16 14:44:16 +0300

    add assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { HeroSection } from "@/components/root/HeroSection";
 
 
 export default function Home() {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-	
+		<section className="flex flex-col items-center">
+		<HeroSection/>
 		</section>
 	);
 }
--- src/components/root/HeroSection.tsx
+++ src/components/root/HeroSection.tsx
@@ -1,11 +1,26 @@
+import Image from "next/image";
+
 interface HeroSectionProps {
 
 }
 
 export function HeroSection({}:HeroSectionProps){
 return (
- <div className='w-full h-full flex items-center justify-center'>
-
- </div>
+  <div className="w-full h-screen flex items-center justify-center relative">
+    <Image
+      className="w-full h-full  "
+      src="/assets/land-big.webp"
+      alt="Mashamba Splash screen"
+      width={500}
+      height={500}
+    />
+    <div
+      className=" w-full  z-3 absolute top-0 bottom-0 
+    flex flex-col md:flex-row items-center justify-center md:justify-evenly gap-5">
+      <div className="text-5xl font-bold flex bg-opacity-40 bg-slate-700 p-5">
+        <div>find your dream property with us</div>
+      </div>
+    </div>
+  </div>
 );
 }
